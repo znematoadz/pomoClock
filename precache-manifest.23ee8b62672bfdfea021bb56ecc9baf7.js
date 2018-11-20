1ca303b4b924cc58cae51a35975d197296f964d8
Alex J <jones6211@gmail.com>
2018-11-20 13:20:54 -0600

    Updates

---
+++ precache-manifest.23ee8b62672bfdfea021bb56ecc9baf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "129b371b9ebb8c103fcb",
+    "url": "/pomoClock/static/js/runtime~main.129b371b.js"
+  },
+  {
+    "revision": "8a29867e5d03cb7da33e",
+    "url": "/pomoClock/static/js/main.8a29867e.chunk.js"
+  },
+  {
+    "revision": "4a919ec15b1c1fd2d613",
+    "url": "/pomoClock/static/js/1.4a919ec1.chunk.js"
+  },
+  {
+    "revision": "8a29867e5d03cb7da33e",
+    "url": "/pomoClock/static/css/main.4ced708e.chunk.css"
+  },
+  {
+    "revision": "4a919ec15b1c1fd2d613",
+    "url": "/pomoClock/static/css/1.a7933a6d.chunk.css"
+  },
+  {
+    "revision": "a55fcd0271ed4910177195083b8bebf3",
+    "url": "/pomoClock/index.html"
+  }
+];
